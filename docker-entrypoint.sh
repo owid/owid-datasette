1f615b1d7af01f3d51ead041105bf2bcf29cfc3f
Marcel Gerber <mgerber59@gmail.com>
2022-08-05 13:21:55 +0200

    fix: don't go through cloudflare cache

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,5 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-wget -O - https://files.ourworldindata.org/owid.db.gz | gunzip -c > /tmp/owid.db
+wget -O - https://files.ourworldindata.org/owid.db.gz?nocache | gunzip -c > /tmp/owid.db
 datasette -h 0.0.0.0 --cors -m metadata.yml --setting sql_time_limit_ms 1800 --setting default_cache_ttl 3600 /tmp/owid.db
