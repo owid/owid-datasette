2aa4a89de2c2134161f5abebdd84bacd05e49b7d
Daniel Bachler <daniel@danielbachler.de>
2023-04-18 18:15:28 +0200

    Add posts_gdocs and --keep-confidential-data flag

--- sqlite-from-mysql.sh
+++ sqlite-from-mysql.sh
@@ -10,7 +10,24 @@ rm owid.db.gz || true
 # pipe this into mysql2sqlite and pipe this into sqlite3 to created owid.db
 mysqldump --skip-extended-insert --no-tablespaces --column-statistics=0 --compact --port \
     $DB_PORT --password=$DB_PASSWD --user $DB_USER --host $DB_HOST $DB_NAME \
-    users variables tags sources posts post_tags namespaces datasets dataset_tags charts chart_tags chart_slug_redirects \
+    users \
+    variables \
+    tags \
+    sources \
+    posts \
+    post_tags \
+    namespaces \
+    datasets \
+    dataset_tags \
+    charts \
+    chart_tags \
+    chart_slug_redirects \
+    chart_dimensions \
+    details \
+    posts_gdocs \
+    pageviews \
+    images \
+    posts_gdocs_x_images \
   | ./mysql2sqlite - \
   | sqlite3 owid.db
 # Run the postprocess-db python script. This is the place to censor some rows or add views etc
