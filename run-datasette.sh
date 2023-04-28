9c236602d713bd80e7e1b6107bf42dbc4276f83f
Daniel Bachler <daniel@danielbachler.de>
2023-04-28 21:48:41 +0200

    Add poetry and prepare private instance

---
+++ run-datasette.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env  bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+poetry run datasette -h 0.0.0.0 --cors .
--- sqlite-from-mysql.sh
+++ sqlite-from-mysql.sh
@@ -5,6 +5,8 @@ set -o nounset
 
 # Remove the old file if there is one
 rm owid.db || true
+rm owid-public.db || true
+rm owid-private.db || true
 rm owid.db.gz || true
 # Run mysqldump with a subset of tables and some flags to bring it into the required format, the
 # pipe this into mysql2sqlite and pipe this into sqlite3 to created owid.db
@@ -31,11 +33,14 @@ mysqldump --skip-extended-insert --no-tablespaces --column-statistics=0 --compac
     posts_gdocs_links \
     posts_gdocs_x_tags \
   | ./mysql2sqlite - \
-  | sqlite3 owid.db
+  | sqlite3 owid-public.db
 # Run the postprocess-db python script. This is the place to censor some rows or add views etc
-python process-db/postprocess-db.py owid.db
-python process-db/extract-links.py owid.db
+cp owid-public.db owid-private.db
+python process-db/postprocess-db.py owid-public.db
+python process-db/extract-links.py owid-public.db
+python process-db/postprocess-db.py --keep-confidential-data owid-private.db
+python process-db/extract-links.py owid-private.db
 # Gzip the file
-gzip -9 -c owid.db > owid.db.gz
+gzip -9 -c owid-public.db > owid.db.gz
 # And upload to s3
 s3cmd put -P owid.db.gz s3://owid-public
