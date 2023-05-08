78e22097c9195697eebb7ff5dec7d5a2359992b5
Marcel Gerber <mgerber59@gmail.com>
2023-05-09 00:20:54 +0200

    fix: simplify json_unquote(json_extract()) calls for sqlite

--- sqlite-from-mysql.sh
+++ sqlite-from-mysql.sh
@@ -17,6 +17,8 @@ set +o allexport
 # pipe this into mysql2sqlite and pipe this into sqlite3 to create owid.db.
 # we need the additional `sed` command to replace string literals like `_utf8mb4'$.slug'`, which
 # mysql generates for generated columns, with just `$.slug`.
+# we also need to replace `JSON_UNQUOTE(JSON_EXTRACT())` with just `JSON_EXTRACT()`, because
+# always does JSON_UNQUOTE(), but doesn't know the function name.
 mysqldump --skip-extended-insert --no-tablespaces --column-statistics=0 --compact --port \
     $DB_PORT --password=$DB_PASSWD --user $DB_USER --host $DB_HOST $DB_NAME \
     users \
@@ -41,6 +43,7 @@ mysqldump --skip-extended-insert --no-tablespaces --column-statistics=0 --compac
     posts_gdocs_x_tags \
   | ./mysql2sqlite - \
   | sed 's/_utf8mb4//g' \
+  | sed 's/JSON_UNQUOTE(JSON_EXTRACT(/(JSON_EXTRACT(/gi' \
   | sqlite3 owid-public.db
 # Run the postprocess-db python script. This is the place to censor some rows or add views etc
 cp owid-public.db owid-private.db
