229dcdaf8b1db71197e506783ff261dee438a012
Marigold <mojmir.vinkler@gmail.com>
2023-08-17 18:42:13 +0200

    :sparkles: push nightly rebuilds to repo

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
-# This script rebuilds the sqlite file and restarts the datasette systemd service
+# This script rebuilds the sqlite file, triggers empty commit and restarts the datasette systemd service
 git pull
 ./sqlite-from-mysql.sh
+git pull
+git commit --allow-empty -m "Nightly rebuild"
+git push
 sudo systemctl restart datasette
