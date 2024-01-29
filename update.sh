e45a4b5f73bd649b6fb2ad3ac948a743a0e85709
Marigold <mojmir.vinkler@gmail.com>
2024-01-29 16:22:37 +0100

    :hammer: move code for generating SQLite to analytics

--- update.sh
+++ update.sh
@@ -1,7 +1,16 @@
 #!/bin/bash -e
-# This script rebuilds the sqlite file, triggers empty commit and restarts the datasette systemd service
-git pull
-./sqlite-from-mysql.sh
+# This script fetches SQLite databases, triggers empty commit and restarts the datasette systemd service
+
+# Rename generated SQLite database to owid.db for backwards compatibility
+s3cmd cp s3://owid-public/owid-public.sqlite.gz s3://owid-public/owid.db.gz
+
+# Fetch private database
+# NOTE: one-liner doesn't work for some reason
+# s3cmd get s3://owid/owid-private.sqlite.gz - | gunzip > owid.db
+s3cmd get s3://owid/owid-private.sqlite.gz
+gunzip owid-private.sqlite.gz
+mv owid-private.sqlite owid.db
+
 git pull
 git commit --allow-empty -m "Nightly rebuild"
 git push
