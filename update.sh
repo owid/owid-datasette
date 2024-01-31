bd386ce8d2eea153d476daa8c42fc31cef4f4dd9
Marigold <mojmir.vinkler@gmail.com>
2024-01-31 09:58:53 +0100

    add note about metadata.yml

--- update.sh
+++ update.sh
@@ -11,6 +11,17 @@ s3cmd get s3://owid/owid-private.sqlite.gz
 gunzip owid-private.sqlite.gz
 mv owid-private.sqlite owid.db
 
+# NOTE: it's not yet clear whether it'd be more useful to have metadata.yml in the analytics repository close to
+#   actual tables or in the owid-datasette repository close to Datasette. For now we'll keep it in the analytics
+# Copy metadata.yml from analytics repository
+# (
+#     cd /home/owid
+#     git clone git@github.com:owid/analytics.git || true
+#     cd analytics
+#     git pull
+#     cp snail/metadata.yml /home/owid/owid-datasette/metadata.yml
+# )
+
 git pull
 git commit --allow-empty -m "Nightly rebuild"
 git push
