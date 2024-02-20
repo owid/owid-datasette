ff34e14ffe24b16cfc834c220c84a54bdfad5e82
Lars Yencken <lars@yencken.org>
2024-02-20 13:58:51 +0000

    :honeybee: Clarify that update.sh is not for local dev

--- update.sh
+++ update.sh
@@ -1,28 +1,41 @@
 #!/bin/bash -e
 # This script fetches SQLite databases, triggers empty commit and restarts the datasette systemd service
 
-# Rename generated SQLite database to owid.db for backwards compatibility
-s3cmd cp s3://owid-public/owid-public.sqlite.gz s3://owid-public/owid.db.gz
+sanity_check() {
+    # only meant to be run on Ubuntu 22.04 server, not in local dev
+    if ! which lsb_release &>/dev/null || [[ "$(lsb_release -is)" != "Ubuntu" ]]; then
+        echo "ERROR: this script is meant to be run on the datasette-private server in prod"
+        exit 1
+    fi
+}
 
-# Fetch private database
-# NOTE: one-liner doesn't work for some reason
-# s3cmd get s3://owid/owid-private.sqlite.gz - | gunzip > owid.db
-s3cmd get s3://owid/owid-private.sqlite.gz
-gunzip owid-private.sqlite.gz
-mv owid-private.sqlite owid.db
+run_update() {
+    # Rename generated SQLite database to owid.db for backwards compatibility
+    s3cmd cp s3://owid-public/owid-public.sqlite.gz s3://owid-public/owid.db.gz
 
-# NOTE: it's not yet clear whether it'd be more useful to have metadata.yml in the analytics repository close to
-#   actual tables or in the owid-datasette repository close to Datasette. For now we'll keep it in the analytics
-# Copy metadata.yml from analytics repository
-# (
-#     cd /home/owid
-#     git clone git@github.com:owid/analytics.git || true
-#     cd analytics
-#     git pull
-#     cp snail/metadata.yml /home/owid/owid-datasette/metadata.yml
-# )
+    # Fetch private database
+    # NOTE: one-liner doesn't work for some reason
+    # s3cmd get s3://owid/owid-private.sqlite.gz - | gunzip > owid.db
+    s3cmd get s3://owid/owid-private.sqlite.gz
+    gunzip owid-private.sqlite.gz
+    mv owid-private.sqlite owid.db
 
-git pull
-git commit --allow-empty -m "Nightly rebuild"
-git push
-sudo systemctl restart datasette
+    # NOTE: it's not yet clear whether it'd be more useful to have metadata.yml in the analytics repository close to
+    #   actual tables or in the owid-datasette repository close to Datasette. For now we'll keep it in the analytics
+    # Copy metadata.yml from analytics repository
+    # (
+    #     cd /home/owid
+    #     git clone git@github.com:owid/analytics.git || true
+    #     cd analytics
+    #     git pull
+    #     cp snail/metadata.yml /home/owid/owid-datasette/metadata.yml
+    # )
+
+    git pull
+    git commit --allow-empty -m "Nightly rebuild"
+    git push
+    sudo systemctl restart datasette
+}
+
+sanity_check
+run_update
