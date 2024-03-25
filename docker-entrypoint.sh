bf3c12f382fa7fd53a31b64a470ec922a2e17f19
Marigold <mojmir.vinkler@gmail.com>
2024-03-25 12:03:21 +0100

    :honeybee: migrate datasette-public from DigitalOcean to LXC container

--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env  bash
-set -o errexit
-set -o pipefail
-set -o nounset
-
-wget -O - https://files.ourworldindata.org/owid.db.gz?nocache | gunzip -c > /tmp/owid.db
-datasette -h 0.0.0.0 --cors -m metadata.yml --setting sql_time_limit_ms 4000 --setting default_cache_ttl 3600 --setting max_returned_rows 6000 --setting facet_time_limit_ms 1000  /tmp/owid.db --static assets:static-files/
