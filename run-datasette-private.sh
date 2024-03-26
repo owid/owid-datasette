cc8d7a6ee0a8e46a0d6b87a2f1517d35ef203f17
Marigold <mojmir.vinkler@gmail.com>
2024-03-26 07:54:24 +0100

    :bug: change paths to dbs

--- run-datasette-private.sh
+++ run-datasette-private.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-poetry run datasette owid-private.db -h 0.0.0.0 -p 8001 --static assets:static-files/ --cors
+poetry run datasette ../private-db/owid.db -h 0.0.0.0 -p 8001 --static assets:static-files/ --cors
