297011e6dc1ce1534fc7f34dbac113e779f35113
Lars Yencken <lars@yencken.org>
2024-07-09 20:21:14 +0100

    :tada: Allow joins between analytics and private dbs

--- run-datasette-private.sh
+++ run-datasette-private.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-poetry run datasette ../private-db/owid.db ../private-db/analytics.db -h 0.0.0.0 -p 8001 --static assets:static-files/ --cors -m metadata.yml --setting sql_time_limit_ms 4000 --setting default_cache_ttl 3600 --setting max_returned_rows 6000 --setting facet_time_limit_ms 1000
+poetry run datasette ../private-db/owid.db ../private-db/analytics.db -h 0.0.0.0 -p 8001 --static assets:static-files/ --crossdb --cors -m metadata.yml --setting sql_time_limit_ms 4000 --setting default_cache_ttl 3600 --setting max_returned_rows 6000 --setting facet_time_limit_ms 1000
